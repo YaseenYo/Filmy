2b553a3c346839515dbfc498ce0aa5e2bad6e962
yaseenyo <mohammed17yo@gmail.com>
2022-02-21 23:58:28 +0530

    update connection string

--- src/api/moviesapi.js
+++ src/api/moviesapi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL : "https://moviesapi.azurewebsites.net/api",
+    baseURL : "https://1da18cs086moviesapi.azurewebsites.net/api",
 });
